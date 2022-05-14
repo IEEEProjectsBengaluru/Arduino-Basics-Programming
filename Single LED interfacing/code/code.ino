4f1301fc203ef92a77b9096e27a1310aef6cba84
IEEE Projects Bengaluru <105450161+IEEEProjectsBengaluru@users.noreply.github.com>
2022-05-14 12:15:38 +0530

    Add files via upload

---
+++ Single LED interfacing/code/code.ino
@@ -0,0 +1,31 @@
+/* This program bliks the led connected to Arduino for one second, repeatedly.
+
+ * Hardware Component :  
+ *      Arduino Uno 
+ *      Led Bargraph
+ * 
+ * Circuit info: 
+ *      D8 of Arduino Uno are connected to a LED  
+ *
+ * Author : IEEE Projects Bengaluru
+ * 
+ * Contact us: 
+ *     Website: https://ieeeprojectsbengaluru.godaddysites.com/ 
+ *     Youtube Channel: https://www.youtube.com/channel/UCKEkm5M_eVhb_NLZtv-M8MA 
+ *
+ */
+
+#define LEDPIN 8 
+
+void setup() {
+  // initialize digital pin D8 as an output.
+  pinMode(LEDPIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LEDPIN, HIGH);   // turn the LED on 
+  delay(1000);                       // wait for a second
+  digitalWrite(LEDPIN, LOW);    // turn the LED off 
+  delay(1000);                       // wait for a second
+}
